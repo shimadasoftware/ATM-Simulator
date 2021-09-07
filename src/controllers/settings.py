88097fb929e4e4e37884e2296a50504d6ba1e5b3
shimadasoftware <73977456+shimadasoftware@users.noreply.github.com>
2021-09-07 02:20:08 -0500

    Delete src directory

--- src/controllers/settings.py
+++
@@ -1,8 +0,0 @@
-import pathlib
-from types import SimpleNamespace
-from pyflexconfig import bootstrap
-
-config = SimpleNamespace()
-
-_default_config_path = pathlib.Path(__file__).resolve().parent / "../config/defaultSettings.py"
-bootstrap(config, defaults_path=_default_config_path)
--- src/main.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-from controllers.mainController import mainController
-
-if __name__ == "__main__":
-    mainController(False)
